c1b083322959ccdfa7fb6c5680209279692fb1a7
Raphael <rparodi@student.42.fr>
2025-10-03 13:31:35 +0200

    test

--- src/auth/src/routes/logout.ts
+++ src/auth/src/routes/logout.ts
@@ -3,7 +3,6 @@ import { FastifyPluginAsync } from 'fastify';
 const route: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
 	void _opts;
 	fastify.post(
-		'/api/auth/logout',
 		async function(_req, res) {
 			void _req;
 			return res.clearCookie('token').send('{}');
