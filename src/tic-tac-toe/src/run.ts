c7149cffad197b52837e5b7457028932e566ad99
apetitco <apetitco@student.42.fr>
2025-12-04 15:05:49 +0100

    (misc): Started working on entrypoint for the microservice.

---
+++ src/tic-tac-toe/src/run.ts
@@ -0,0 +1,30 @@
+// @file run.ts
+
+// Entry point of the microservice, ran by the Dockerfile.
+
+import fastify, { FastifyInstance } from 'fastify';
+// TODO: Import the microservice app
+
+
+// @brief Entrypoint for the microservice's backend.
+const start = async () => {
+	// TODO: Thingies to send to log service (if I understood that correctly from /src/chat/src/run.ts)
+
+	// TODO: Add the logging thingy to the call to fastify()
+	const fastInst: FastifyInstance = fastify();
+	try {
+		process.on('SIGTERM', () => {
+			fastInst.log.info('Requested to shutdown');
+			process.exit(143);
+		});
+		// TODO: Uncomment when app.ts will be import-able.
+		// await fastInst.register(app);
+		await fastInst.listen({ port: 80, host: '0.0.0.0' });
+	}
+	catch (err) {
+		fastInst.log.error(err);
+		process.exit(1);
+	};
+};
+
+start();
