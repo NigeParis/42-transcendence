53a2c06d43120b43395ae9f69f0dba609bfca291
Maieul BOYER <maieul.dev@familleboyer.net>
2025-12-16 15:20:04 +0100

    fixed things
    # Conflicts:
    #	src/pnpm-lock.yaml

--- src/tic-tac-toe/src/app.ts
+++ src/tic-tac-toe/src/app.ts
@@ -1,56 +1,101 @@
-import { FastifyInstance, FastifyPluginAsync } from 'fastify';
-import fastifySocketIO from 'fastify-socket.io';
 import { TTC } from './game';
+import { FastifyInstance, FastifyPluginAsync } from 'fastify';
+import fastifyFormBody from '@fastify/formbody';
+import fastifyMultipart from '@fastify/multipart';
+import * as db from '@shared/database';
+import * as auth from '@shared/auth';
+import * as swagger from '@shared/swagger';
+import * as utils from '@shared/utils';
+import { Server } from 'socket.io';
 
-const app: FastifyPluginAsync = async (fastify: FastifyInstance, opts): Promise<void> => {
+declare const __SERVICE_NAME: string;
+
+// @ts-expect-error: import.meta.glob is a vite thing. Typescript doesn't know this...
+const plugins = import.meta.glob('./plugins/**/*.ts', { eager: true });
+// @ts-expect-error: import.meta.glob is a vite thing. Typescript doesn't know this...
+const routes = import.meta.glob('./routes/**/*.ts', { eager: true });
+
+const app: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
 	void opts;
-	await fastify.register(fastifySocketIO, {
-		cors: {
-			origin: '*',
-			methods: ['GET', 'POST'],
-		},
-	});
+
+	await fastify.register(utils.useMonitoring);
+	await fastify.register(utils.useMakeResponse);
+	await fastify.register(swagger.useSwagger, { service: __SERVICE_NAME });
+	await fastify.register(db.useDatabase as FastifyPluginAsync, {});
+	await fastify.register(auth.jwtPlugin as FastifyPluginAsync, {});
+	await fastify.register(auth.authPlugin as FastifyPluginAsync, {});
+
+	// Place here your custom code!
+	for (const plugin of Object.values(plugins)) {
+		void fastify.register(plugin as FastifyPluginAsync, {});
+	}
+	for (const route of Object.values(routes)) {
+		void fastify.register(route as FastifyPluginAsync, {});
+	}
+
+	void fastify.register(fastifyFormBody, {});
+	void fastify.register(fastifyMultipart, {});
 
 	const game = new TTC();
 
-	fastify.ready().then(() => {
-		fastify.io.on('connection', (socket) => {
-			fastify.log.info(`Client connected: ${socket.id}`);
+	fastify.ready((err) => {
+		if (err) throw err;
+		onReady(fastify, game);
+	});
+};
+export default app;
+export { app };
 
-			socket.emit('gameState', {
-				board: game.board,
-				turn: game.currentPlayer,
-				gameOver: game.isGameOver,
-			});
+// When using .decorate you have to specify added properties for Typescript
+declare module 'fastify' {
+	interface FastifyInstance {
+		io: Server<{
+			hello: (message: string) => string;
+			// idk you put something
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			gameState: any;
+			makeMove: (idx: number) => void;
+			resetGame: () => void;
+			error: string,
+		}>;
+	}
+}
 
-			socket.on('makeMove', (idx: number) => {
-				const result = game.makeMove(idx);
+async function onReady(fastify: FastifyInstance, game: TTC) {
+	fastify.io.on('connection', (socket) => {
+		fastify.log.info(`Client connected: ${socket.id}`);
 
-				if (result === 'invalidMove') {
-					socket.emit('error', 'Invalid Move');
-				}
-				else {
-					fastify.io.emit('gameState', {
-						board: game.board,
-						turn: game.currentPlayer,
-						lastResult: result,
-					});
-				}
-			});
+		socket.emit('gameState', {
+			board: game.board,
+			turn: game.currentPlayer,
+			gameOver: game.isGameOver,
+		});
 
-			socket.on('resetGame', () => {
-				game.reset();
+		socket.on('makeMove', (idx: number) => {
+			const result = game.makeMove(idx);
+
+			if (result === 'invalidMove') {
+				socket.emit('error', 'Invalid Move');
+			}
+			else {
 				fastify.io.emit('gameState', {
 					board: game.board,
 					turn: game.currentPlayer,
-					reset: true,
+					lastResult: result,
 				});
+			}
+		});
+
+		socket.on('resetGame', () => {
+			game.reset();
+			fastify.io.emit('gameState', {
+				board: game.board,
+				turn: game.currentPlayer,
+				reset: true,
 			});
 		});
 	});
-};
-
-export default app;
+}
 
 // // TODO: Import the plugins defined for this microservice
 // // TODO: Import the routes defined for this microservice
@@ -238,3 +283,4 @@ export default app;
 // // 		});
 // // 	});
 // // }
+
---
+++ src/tic-tac-toe/src/plugins/socket.ts
@@ -0,0 +1,31 @@
+import type {
+	FastifyInstance,
+	FastifyPluginAsync,
+	HookHandlerDoneFunction,
+} from 'fastify';
+import fp from 'fastify-plugin';
+import { Server } from 'socket.io';
+
+const F: (
+	f: FastifyInstance,
+) => Omit<FastifyInstance, 'io'> & { io: Server } = (f) =>
+		f as Omit<FastifyInstance, 'io'> & { io: Server };
+
+const fastifySocketIO: FastifyPluginAsync = fp(async (fastify) => {
+	function defaultPreClose(done: HookHandlerDoneFunction) {
+		F(fastify).io.local.disconnectSockets(true);
+		done();
+	}
+	fastify.decorate(
+		'io',
+		new Server(fastify.server, { path: '/api/chat/socket.io' }),
+	);
+	fastify.addHook('preClose', defaultPreClose);
+	fastify.addHook('onClose', (instance: FastifyInstance, done) => {
+		F(instance).io.close();
+		done();
+	});
+});
+
+export default fastifySocketIO;
+
