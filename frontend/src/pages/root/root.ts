3120a640e336a89f4613d09fb2e80955ecb2b08e
Maieul BOYER <maieul.dev@familleboyer.net>
2025-12-23 15:53:10 +0100

    added a real frontpage

--- frontend/src/pages/root/root.ts
+++ frontend/src/pages/root/root.ts
@@ -1,18 +1,11 @@
-import { addRoute, setTitle, type RouteHandlerParams } from "@app/routing";
-import page from './root.html?raw'
-import { updateUser } from "@app/auth";
+import { addRoute, setTitle } from "@app/routing";
+import page from "./root.html?raw";
 
-addRoute('/', async (_: string): Promise<string> => {
-	let user = await updateUser();
-	if (user === null)
-		setTitle(`Welcome`)
-	else
-		setTitle(`Welcome ${user.guest ? '[GUEST] ' : ''}${user.name}`);
-	return page;
-}, { bypass_auth: true })
-
-addRoute('/with_title/:title', (_: string, args: RouteHandlerParams) => {
-	setTitle(args.title)
-	console.log(`title should be '${args.title}'`);
-	return page;
-}, { bypass_auth: false })
+addRoute(
+	"/",
+	async (_: string): Promise<string> => {
+		setTitle(`Welcome`);
+		return page;
+	},
+	{ bypass_auth: true },
+);
